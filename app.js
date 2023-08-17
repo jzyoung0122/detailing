ff49e203647ddc3b31f266a6cec730e78ca62ce8
jzyoung0122 <142475319+jzyoung0122@users.noreply.github.com>
2023-08-18 02:03:15 +1000

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ app.use(cors());
 require('./routes')(app);
 require('./plugins/db')(app);
 
+app.all('*',function(req,res,next){
+   
+ 
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Methods","PUT,GET,POST,DELETE,OPTIONS");
+    // res.header("Access-Control-Allow-Headers","X-Requestd-With");
+    res.header("Access-Control-Allow-Headers","Content-Type");
+    next()
+});
+
+
 app.get('/', function (req, res) {
   res.setHeader('Access-Control-Allow-Credentials',"true");
   res.send('Received client request');
