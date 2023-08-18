44469e2978c1476438522bf454d4accec4147ea2
jzyoung0122 <142475319+jzyoung0122@users.noreply.github.com>
2023-08-18 21:34:27 +1000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -274,100 +274,100 @@ module.exports=(app)=>{
 
     })
 
-    Description.create({
-        title:"Regular Detailing",
-        description:[
-            "Vacuum Interior (Carpets,  Mats, Seats & Boot )",
-            "Interior Window Cleaned",
-            "Dashboard Cleaned",
-            "Door & Boot Jambs cleaned",
-            "More Extensive Vacuum On Carpet & Mats using powerful tools",
-            "Foam Wash Pre-Rinse (Softens excessive dirt & road grime to reduce the risk of scratching the paint)",
-            "PH Neutral Foam Wash ( Safe For Ceramic Coating )",
-            "Non-Acidic Wheel Clean",
-            "Exterior Windows & Body Dried & Waxed",
-            "Non-Greasy Tyre Shine Applied",
-        ]
-    })
+    // Description.create({
+    //     title:"Regular Detailing",
+    //     description:[
+    //         "Vacuum Interior (Carpets,  Mats, Seats & Boot )",
+    //         "Interior Window Cleaned",
+    //         "Dashboard Cleaned",
+    //         "Door & Boot Jambs cleaned",
+    //         "More Extensive Vacuum On Carpet & Mats using powerful tools",
+    //         "Foam Wash Pre-Rinse (Softens excessive dirt & road grime to reduce the risk of scratching the paint)",
+    //         "PH Neutral Foam Wash ( Safe For Ceramic Coating )",
+    //         "Non-Acidic Wheel Clean",
+    //         "Exterior Windows & Body Dried & Waxed",
+    //         "Non-Greasy Tyre Shine Applied",
+    //     ]
+    // })
 
-    Description.create({
-        title:"Interior Detailing",
-        description:[
-            "Vacuum Interior (Carpets,  Mats, Seats & Boot )",
-            "Excessive Animal Hair & Sand Removed ",
-            "Sterilizes the interior with steam to eliminate unwanted germs & bacteria",
-            "Steam clean Doors",
-            "Steam clean All Cup-holders, Dashboard & Console.",
-            "Shampoo & extraction of carpet & seats",
-            "Shampoo the mats and washed ",
-            "Lether seats steam cleaned and conditioned",
-            "Protection to all interior panels ( Dash | Console | Door panels | Cup-holders )",
-            "Interior Window Cleaned",
-        ]
-    })
+    // Description.create({
+    //     title:"Interior Detailing",
+    //     description:[
+    //         "Vacuum Interior (Carpets,  Mats, Seats & Boot )",
+    //         "Excessive Animal Hair & Sand Removed ",
+    //         "Sterilizes the interior with steam to eliminate unwanted germs & bacteria",
+    //         "Steam clean Doors",
+    //         "Steam clean All Cup-holders, Dashboard & Console.",
+    //         "Shampoo & extraction of carpet & seats",
+    //         "Shampoo the mats and washed ",
+    //         "Lether seats steam cleaned and conditioned",
+    //         "Protection to all interior panels ( Dash | Console | Door panels | Cup-holders )",
+    //         "Interior Window Cleaned",
+    //     ]
+    // })
 
-    Description.create({
-        title:"Exterior Detailing",
-        description:[
-            "Foam Wash Pre-Rinse (Softens excessive dirt & road grime to reduce the risk of scratching the paint)",
-            "PH Neutral Foam Wash ( Safe For Ceramic Coating )",
-            "Non-Acidic Wheel Clean & Detailed",
-            "HAO'S Signature Decontamination Treatment (iron removal with clay bar finish)",
-            "Exterior Windows & Body Dried",
-            "Hand or Machine polish",
-            "Paint enhancement with Hydrophobic & SiO2 coating, which delivers EXTREME  ",
-            "water beading & chemical resistant protection",
-            "Plastics & rubbers glossed",
-            "Non-Greasy Tyre Shine Applied",
-        ]
-    })
+    // Description.create({
+    //     title:"Exterior Detailing",
+    //     description:[
+    //         "Foam Wash Pre-Rinse (Softens excessive dirt & road grime to reduce the risk of scratching the paint)",
+    //         "PH Neutral Foam Wash ( Safe For Ceramic Coating )",
+    //         "Non-Acidic Wheel Clean & Detailed",
+    //         "HAO'S Signature Decontamination Treatment (iron removal with clay bar finish)",
+    //         "Exterior Windows & Body Dried",
+    //         "Hand or Machine polish",
+    //         "Paint enhancement with Hydrophobic & SiO2 coating, which delivers EXTREME  ",
+    //         "water beading & chemical resistant protection",
+    //         "Plastics & rubbers glossed",
+    //         "Non-Greasy Tyre Shine Applied",
+    //     ]
+    // })
 
-    Description.create({
-        title:"Full Detailing",
-        description:[
-            "Vacuum Interior (Carpets, Mats, Seats & Boot )",
-            "Excessive Animal Hair & Sand Removed ",
-            "Shampoo & extraction of carpet & seats",
-            "Lether seats cleaned and conditioned",
-            "Clean all interior panels ( Dash | Console | Door panels | Cup-holders )",
-            "Interior Window Cleaned",
-            "Foam Wash Pre-Rinse (Softens excessive dirt & road grime to reduce the risk of scratching the paint)",
-            "PH Neutral Foam Wash ( Safe For Ceramic Coating )",
-            "Non-Acidic Wheel Clean & Detailed",
-            "HAO'S Signature Decontamination Treatment (iron removal with clay bar finish)",
-            "Exterior Windows & Body Dried",
-            "Hand or Machine polish",
-            "Paint enhancement with Hydrophobic & SiO2 coating, which delivers EXTREME  ",
-            "water beading & chemical resistant protection",
-            "Plastics & rubbers glossed",
-            "Non-Greasy Tyre Shine Applied",
-        ]
-    })
+    // Description.create({
+    //     title:"Full Detailing",
+    //     description:[
+    //         "Vacuum Interior (Carpets, Mats, Seats & Boot )",
+    //         "Excessive Animal Hair & Sand Removed ",
+    //         "Shampoo & extraction of carpet & seats",
+    //         "Lether seats cleaned and conditioned",
+    //         "Clean all interior panels ( Dash | Console | Door panels | Cup-holders )",
+    //         "Interior Window Cleaned",
+    //         "Foam Wash Pre-Rinse (Softens excessive dirt & road grime to reduce the risk of scratching the paint)",
+    //         "PH Neutral Foam Wash ( Safe For Ceramic Coating )",
+    //         "Non-Acidic Wheel Clean & Detailed",
+    //         "HAO'S Signature Decontamination Treatment (iron removal with clay bar finish)",
+    //         "Exterior Windows & Body Dried",
+    //         "Hand or Machine polish",
+    //         "Paint enhancement with Hydrophobic & SiO2 coating, which delivers EXTREME  ",
+    //         "water beading & chemical resistant protection",
+    //         "Plastics & rubbers glossed",
+    //         "Non-Greasy Tyre Shine Applied",
+    //     ]
+    // })
 
-    Description.create({
-        title:"Pre-sale Detailing or usedcar buyer's option",
-        description:[
-            "Vacuum Interior (Carpets, Mats, Seats & Boot )",
-            "Excessive Animal Hair & Sand Removed ",
-            "Sterilizes the interior with steam to eliminate unwanted germs & bacteria",
-            "Steam clean Doors",
-            "Steam clean All Cup-holders, Dashboard & Console",
-            "Shampoo & extraction of carpet & seats",
-            "Lether seats steam cleaned and conditioned",
-            "Protection to all interior panels ( Dash | Console | Door panels | Cup-holders )",
-            "Interior Window Cleaned",
-            "Foam Wash Pre-Rinse (Softens excessive dirt & road grime to reduce the risk of scratching the paint)",
-            "PH Neutral Foam Wash ( Safe For Ceramic Coating )",
-            "Non-Acidic Wheel Clean & Detailed",
-            "Engine bay cleaned",
-            "HAO’S Signature Decontamination Treatment (iron removal with clay bar finish)",
-            "Exterior Windows & Body Dried",
-            "Hand or Machine polish",
-            "Paint enhancement with Hydrophobic & SiO2 coating, which delivers EXTREME ",
-            "water beading & chemical resistant protection",
-            "Plastics & rubbers glossed",
-            "Non-Greasy Tyre Shine Applied"
-        ]
+    // Description.create({
+    //     title:"Pre-sale Detailing or usedcar buyer's option",
+    //     description:[
+    //         "Vacuum Interior (Carpets, Mats, Seats & Boot )",
+    //         "Excessive Animal Hair & Sand Removed ",
+    //         "Sterilizes the interior with steam to eliminate unwanted germs & bacteria",
+    //         "Steam clean Doors",
+    //         "Steam clean All Cup-holders, Dashboard & Console",
+    //         "Shampoo & extraction of carpet & seats",
+    //         "Lether seats steam cleaned and conditioned",
+    //         "Protection to all interior panels ( Dash | Console | Door panels | Cup-holders )",
+    //         "Interior Window Cleaned",
+    //         "Foam Wash Pre-Rinse (Softens excessive dirt & road grime to reduce the risk of scratching the paint)",
+    //         "PH Neutral Foam Wash ( Safe For Ceramic Coating )",
+    //         "Non-Acidic Wheel Clean & Detailed",
+    //         "Engine bay cleaned",
+    //         "HAO’S Signature Decontamination Treatment (iron removal with clay bar finish)",
+    //         "Exterior Windows & Body Dried",
+    //         "Hand or Machine polish",
+    //         "Paint enhancement with Hydrophobic & SiO2 coating, which delivers EXTREME ",
+    //         "water beading & chemical resistant protection",
+    //         "Plastics & rubbers glossed",
+    //         "Non-Greasy Tyre Shine Applied"
+    //     ]
     })
 
     
