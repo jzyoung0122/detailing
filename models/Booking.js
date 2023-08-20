21ce73f10ed9f89bda491f79d391ef66382c6d1f
jzyoung0122 <142475319+jzyoung0122@users.noreply.github.com>
2023-08-21 00:37:51 +1000

    Add files via upload

---
+++ models/Booking.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    date:Date,
+    time:Number,
+    sedanServices:[],
+    suvServices:[],
+    vanServices:[],
+    firstName:String,
+    lastName:String,
+    phoneNumber:String,
+    email:String,
+    address:{
+        address1:String,
+        address2:String,
+        suburb:String,
+        postCode:String
+    },
+    delete:Boolean,
+    totalTime:Number
+
+});
+
+// 实例化模型
+const Booking = mongoose.model('Booking', schema);
+
+// 导出模型
+module.exports = Booking;
